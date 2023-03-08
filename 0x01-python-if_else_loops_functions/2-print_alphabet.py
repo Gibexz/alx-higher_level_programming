a1919be2b01427abaa635a0d8017000cee55d17a
Gibexz <gibexzetedu@gmail.com>
2023-03-08 16:10:23 +0000

    Task 2 and 3 solution

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 i = 97
-while i < 122:
+while i <= 122:
     print("{0:c}".format(i), end="")
     i += 1
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 i = 97
-while i < 122:
+while i <= 122:
     if i == 101 or i == 113:
         i += 1
     print("{0:c}".format(i), end="")
