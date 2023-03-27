3d03b6c02475c1866e28f8b418e68e2c7d3d53e9
Gibexz <andrewetedu@gmail.com>
2023-03-27 23:50:52 +0000

    Task 2 solution

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    b = 0
+
+    for i in range(x):
+    #while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            b += 1
+        except (ValueError, TypeError):
+            pass
+
+    print()
+    return b
+
