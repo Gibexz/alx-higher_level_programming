35a99f9d50711531b239dbda276174f9489c3a28
Gibexz <andrewetedu@gmail.com>
2023-03-28 00:03:28 +0000

    Task 3 solution and prev tasks pycodestyle corrections

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-#if __name__ == "__main__":
 def safe_print_list(my_list=[], x=0):
     i = 0
     try:
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,6 @@ def safe_print_list_integers(my_list=[], x=0):
     b = 0
 
     for i in range(x):
-    #while i < x:
         try:
             print("{:d}".format(my_list[i]), end="")
             b += 1
@@ -13,4 +12,3 @@ def safe_print_list_integers(my_list=[], x=0):
 
     print()
     return b
-
