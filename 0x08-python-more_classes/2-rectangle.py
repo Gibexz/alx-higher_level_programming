0f225cb1837073ba2446b58ef2c2bf6d8ef0ff88
Gibexz <andrewetedu@gmail.com>
2023-04-03 23:08:47 +0000

    Task 2 corrections

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -9,16 +9,8 @@ class Rectangle():
     class that defines a rectangle
     """
     def __init__(self, width=0, height=0):
-        if type(width) not in [int]:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if type(height) not in [int]:
-            raise TypeError("width must be an integer")
-        if height < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
