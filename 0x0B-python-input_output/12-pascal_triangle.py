d6f515e123ccefaa7bb5b913c9cf18b5d6dda0fe
Gibexz <andrewetedu@gmail.com>
2023-04-13 23:01:12 +0000

    Task 12 Paschal Triangle solution

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -4,4 +4,18 @@
 """
 
 
+def pascal_triangle(n):
+    """
+    function to print pascal triangle
+    """
+    if n <= 0:
+        return []
 
+    triangle = [[1]]
+    for i in range(1, n):
+        ro = [1]
+        for j in range(1, i):
+            ro.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        ro.append(1)
+        triangle.append(ro)
+    return triangle
