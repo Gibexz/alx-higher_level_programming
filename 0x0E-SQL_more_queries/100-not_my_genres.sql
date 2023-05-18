ac18af22ad47f599f46b684ce4ccb9a01a0c44f4
Gibexz <andrewetedu@gmail.com>
2023-05-18 23:32:01 +0100

    Adv Task 17

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -4,7 +4,7 @@ SELECT tv_genres.name -- Query to get genres that not belong
 FROM tv_genres
 LEFT JOIN
 (
-	SELECT tv_genres.id, tv_genres.name -- Query to get Dexter genres
+	SELECT tv_genres.id, tv_genres.name -- ""Query to get Dexter genres""
 	FROM tv_genres
 	JOIN tv_show_genres
 	ON tv_genres.id = tv_show_genres.genre_id
