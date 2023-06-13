6ab2570dfeb81933719ab5b056a2b697eeb78f35
Gibexz <andrewetedu@gmail.com>
2023-06-13 23:03:44 +0100

    Task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,13 +2,13 @@
 
 const args = process.argv;
 let i;
-
+const x =  Math.floor(parseInt(args[2]));
 if (!args[2]) {
   console.log('Missing number of occurrences');
 } else if (isNaN(args[2])) {
   console.log('Missing number of occurrences');
 } else {
-  for (i = 1; i <= Math.floor(parseInt(args[2])); i++) {
+  for (i = 1; i <= x; i++) {
     console.log('C is fun');
   }
 }
