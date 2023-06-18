eaa8bc678704273c7772f02b584bed3080f5f682
Gibexz <andrewetedu@gmail.com>
2023-06-18 14:07:42 +0100

    Task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+script that lists all states with a name starting
+with N (upper N) from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+
+def listStatesN(username, password, database):
+    """ function lists all states with a name starting
+        with N (upper N) from the database hbtn_0e_0_usa """
+
+    """connection to mysqlserver"""
+    dbconnect = (
+            host="local host",
+            port=3306,
+            user="username",
+            passwd="password",
+            db="database",
+            charset="utf8"
+        )
+
+    cur = dbconnect.cursor()
+
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY state.id ASC"
+
+    cur.execute(query)
+
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+
+    cur.close()
+    dbconnect.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[2]
+
+    """runs the script"""
+    listStatesN(username, password, database)
