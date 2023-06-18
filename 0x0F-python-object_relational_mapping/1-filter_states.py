42fa3a9ae964f2d4720a7530d193515db5e4aff0
Gibexz <andrewetedu@gmail.com>
2023-06-18 14:37:05 +0100

    Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -23,9 +23,7 @@ def listStatesN(username, password, database):
 
     cur = dbconnect.cursor()
 
-    query = '''
-        SELECT * FROM states WHERE name LIKE 'N%' ORDER BY state.id ASC
-    '''
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
 
     cur.execute(query)
 
