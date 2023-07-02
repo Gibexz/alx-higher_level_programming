edb8ba5d85301ba2e47d6706ec3ab04a3c17038a
Gibexz <andrewetedu@gmail.com>
2023-07-02 13:50:07 +0100

    Task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+ Python script that takes in a URL, sends a request to the URL and
+ displays the body of the response.
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    req = requests.get(url)
+    status = req.statu_code
+
+    if status < 400:
+        print(req.text)
+    else:
+        print(f"Error code: {status}")
