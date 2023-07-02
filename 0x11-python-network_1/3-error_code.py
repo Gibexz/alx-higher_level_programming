e9cea6261b09ad18d64ea12c918e6793768f3412
Gibexz <andrewetedu@gmail.com>
2023-07-02 13:15:43 +0100

    Task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """
- Python script that takes in a URL and an email, sends a POST request to
- the passed URL with the email as a parameter, and displays the
- body of the response (decoded in utf-8)
+ Python script that takes in a URL, sends a request to the URL and
+ displays the body of the response (decoded in utf-8)
 """
 
 if __name__ == "__main__":
-    from urllib.request import Request, urlopen
-    from urllib.error import HTTPError
+    import urllib.request
     import sys
 
     url = sys.argv[1]
@@ -17,5 +15,5 @@ if __name__ == "__main__":
             content = respon.read()
         print(content.decode("utf-8"))
 
-    except HTTPError as e:
-        print("Error: ", e.code)
+    except urllib.error.HTTPError as e:
+        print("Error:", e.code)
