6069a6e7902cad70227ad6f23b7f70505f811be8
Gibexz <andrewetedu@gmail.com>
2023-07-25 23:31:33 +0100

    Task 2. Status code

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const files = process.argv[2];
+
+fs.readFile(files, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
