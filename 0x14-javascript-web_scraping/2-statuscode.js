acb84a690af02dab9ea31294241a3d370defbea9
Gibexz <andrewetedu@gmail.com>
2023-07-25 23:54:28 +0100

    Task 2. Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,9 +3,7 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, (err, response) => {
-  if (err) {
-    console.error(err);
-  }
-  console.log('code: ', response.statusCode);
-});
+request.get(url)
+	.on('response', (response) => {
+		console.log('code:', response.statusCode);
+	});
